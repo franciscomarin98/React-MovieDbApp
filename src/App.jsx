a48182d455fbace471f6fc55b002cbf0dc714b4e
francisco-1998 <francmarinc@gmail.com>
2021-03-07 13:27:10 -0500

    Configurando un sistema de routes basicas

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,65 @@
-import { HomePage } from './pages/HomePage';
+import {
+  BrowserRouter as Router,
+  Switch, Route
+} from 'react-router-dom';
+import { Layout } from 'antd';
+
+// Pages
+import { HomePage } from './pages/HomePage'
+import { Error404Page } from './pages/Error404Page'
+import { SearchMoviePage } from './pages/SearchMoviePage'
+import { MovieDetailPage } from './pages/MovieDetailPage'
+import { LastReleasesPage } from './pages/LastReleasesPage'
+import { PopularMoviesPage } from './pages/PopularMoviesPage'
 
 export default function App() {
+
+  const { Header, Content } = Layout;
+
   return (
-    <HomePage />
+    <Layout>
+      <Router>
+
+        <Header>
+          Head
+        </Header>
+
+        <Content>
+
+          <Switch>
+
+            <Route path="/" exact={true}  >
+              <HomePage />
+            </Route>
+            
+            <Route path="/last-releases" exact={true}  >
+              <LastReleasesPage />
+            </Route>
+            
+            <Route path="/popular-movies" exact={true}  >
+              <PopularMoviesPage />
+            </Route>
+            
+            <Route path="/search-movies" exact={true}  >
+              <SearchMoviePage />
+            </Route>
+
+            <Route path="/movie-detail/:id" exact={true}>
+              <MovieDetailPage />
+            </Route>
+
+            <Route path="*">
+              <Error404Page />
+            </Route>
+          
+          </Switch>
+
+
+
+        </Content>
+        
+      </Router>
+    </Layout>
   );
 }
 
