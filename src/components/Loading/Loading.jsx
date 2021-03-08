db9ad5e53444b450f0b71d5d91831583b47ec1dd
francisco-1998 <francmarinc@gmail.com>
2021-03-08 09:24:35 -0500

    Implentando loading y animate

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,12 @@
+import { Spin } from 'antd'
+import './Loading.scss'
+
+const Loading = () => {
+    return (
+        <div className="loading">
+            <Spin size='large' />
+        </div>
+    )
+}
+
+export default Loading
--- src/components/SliderMovies/SliderMovies.jsx
+++ src/components/SliderMovies/SliderMovies.jsx
@@ -2,19 +2,20 @@
 import './SliderMovies.scss';
 import { Carousel } from 'antd';
 import ItemSlider from '../ItemSlider';
+import Loading  from '../Loading';
 
 
 const SliderMovies = ({ movies }) => {
 
     if (movies.loading || movies.data === null) {
-        return 'Loading...';
+        return <Loading />
     }
 
     const { results } = movies.data;
     console.log(results);
 
     return (
-        <Carousel autoplay className="slider-movies">
+        <Carousel autoplay className="slider-movies animate__animated animate__fadeIn">
             {
                 results.map(m => (
                     <ItemSlider key={m.id} movie={m}/>
