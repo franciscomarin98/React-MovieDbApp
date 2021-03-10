270630acd0fb91e18b4dfaab7c5e5349652f44a0
francisco-1998 <francmarinc@gmail.com>
2021-03-10 09:21:55 -0500

    Estructurando la pagina de las peliculas populares

--- src/pages/LastReleasesPage.jsx
+++ src/pages/LastReleasesPage.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from 'react';
+import { Row, Col } from 'antd';
+import { baseUrl, apiKey } from '../utils/movieDb';
+import Footer from '../components/Footer/Footer';
+import Loading from '../components/Loading/Loading';
 
 export const LastReleasesPage = () => {
+
+    const [movieList, setMovieList] = React.useState([]);
+    const [page, setPage] = React.useState(1);
+
+    React.useEffect(() => {
+        (async () => {
+
+            const response = await fetch(`${baseUrl}/now_playing?api_key=${apiKey}&language=es-ES&page=${page}`);
+            const movies = await response.json();
+            setMovieList(movies);
+
+        })();
+    }, [page]);
+
     return (
-        <div>
-            Estamos en las ultimas peliculas
-        </div>
+        <Row>
+            <Col span='24' style={{ textAlign: 'center', marginTop: 25 }}>
+                <h1 style={{ fontSize: 35, fontWeight: 'bold' }}>
+                    Últimos lanzamientos
+                </h1>
+            </Col>
+            {movieList.results ? (
+                <Col span="24"></Col>
+            ) : (
+                <Loading />
+            )
+            }
+            <Col span="24">
+                <Footer />
+            </Col>
+        </Row>
     )
 }
