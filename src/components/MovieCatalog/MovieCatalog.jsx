82ee0de3478fbf3b57d5c7ee602e4ea6ae564b58
francisco-1998 <francmarinc@gmail.com>
2021-03-11 18:22:08 -0500

    Se podria decir que fin

--- src/components/MovieCatalog/MovieCatalog.jsx
+++ src/components/MovieCatalog/MovieCatalog.jsx
@@ -17,13 +17,14 @@ const MovieCatalog = ({ movies: { results } }) => {
 const MovieCard = ({ movie }) => {
   const { id, title, poster_path } = movie;
   const { Meta } = Card;
+  const urlImg = baseImgUrl + poster_path;
   return (
     <Link to={`/movie-detail/${id}`}>
       <Card
         className="animate__animated animate__fadeIn"
         hoverable
         style={{ width: 240 }}
-        cover={<img alt={title} src={baseImgUrl + poster_path} />}
+        cover={<img alt={title} src={ (poster_path) ? urlImg : 'https://cdn.bioguia.com/embed/2f92537db76b292d01d5b1977a085551498560800/empty.png' } />}
         actions={[<EyeOutlined />]}
       >
         <Meta title={title} />
--- src/pages/SearchMoviePage/SearchMoviePage.jsx
+++ src/pages/SearchMoviePage/SearchMoviePage.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
 
-const SearchMoviePage = () => {
+import { Row, Col, Input } from 'antd';
+import { useState, useEffect } from 'react';
+import { withRouter } from 'react-router';
+import queryString from 'query-string';
+import MovieCatalog from '../../components/MovieCatalog/MovieCatalog';
+import { apiKey } from '../../utils/movieDb';
+
+import './SearchMoviePage.scss'
+
+
+const SearchMoviePage = ({ location, history }) => {
+    const [moviesSearched, setMoviesSearched] = useState([]);
+    const [searchValue, setSearchValue] = useState('');
+
+    useEffect(() => {
+        (async () => {
+            if(searchValue !== ''){
+                const searchValue = queryString.parseUrl(location.search);
+                const { s } = searchValue.query;
+                const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=es-Es&query=${s}&include_adult=true&page=1`);
+                const data = await response.json();
+                setSearchValue(s);
+                setMoviesSearched(data);
+            }
+        })();
+    }, [location.search]);
+
+    const onChangeSearchValue = e => {
+        const urlParams = queryString.parse(location.search);
+        urlParams.s = e.target.value;
+        history.push(`?${queryString.stringify(urlParams)}`);
+        setSearchValue(e.target.value);
+    }
+
     return (
-        <div>
-            Hola desde el buscador
-        </div>
+        <Row>
+            <Col span="24" className="search">
+                <h1>Encuentra tu pelicula</h1>
+                <Input value={searchValue} onChange={onChangeSearchValue} />
+            </Col>
+            {moviesSearched?.results ? (
+                <Row>
+                    <Col span="24">
+                        <MovieCatalog movies={moviesSearched} />
+                    </Col>
+                </Row>
+            ): null}
+        </Row>
     )
 }
 
-export default SearchMoviePage;
+
+export default withRouter(SearchMoviePage);
